96ff28e3bb392ab496ce8b60e7bd6ad6adc4e4d2
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-02-27 20:27:45 +0530

    refactor: modified core view

--- webfuzz/core/urls.py
+++ webfuzz/core/urls.py
@@ -1,7 +1,7 @@
 """Core URLs"""
 
 from flask import Blueprint
-from .views import IndexView
+from .views import IndexView, serve_favicon_handler
 
 # Create a blueprint for the core module
 blueprint = Blueprint("core", __name__)
@@ -9,3 +9,8 @@ blueprint = Blueprint("core", __name__)
 # Define the URL rules for the core module
 index_view = IndexView.as_view("home")
 blueprint.add_url_rule("/", view_func=index_view)
+
+
+@blueprint.route("/favicon.ico")
+def favicon():
+    return serve_favicon_handler()
--- webfuzz/core/views.py
+++ webfuzz/core/views.py
@@ -1,9 +1,16 @@
 """Core views."""
 
-from flask import render_template
+import os
+from flask import current_app, render_template, send_file
 from flask.views import MethodView
 
 
 class IndexView(MethodView):
     def get(self):
         return render_template("index.html")
+
+
+def serve_favicon_handler():
+    path = os.path.join(current_app.config["APP_DIR"], f"static/public/favicon.ico")
+
+    return send_file(path, mimetype="image/x-icon")
