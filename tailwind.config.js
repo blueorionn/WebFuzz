857079b24dae22fa60a60ce4a9f23f959cf2056f
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-02-27 19:17:09 +0530

    feat: configured tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./webfuzz/templates/**/*.html'],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
