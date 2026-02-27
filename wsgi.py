45d703d55f796255c6187f21af0641a4034a2249
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-02-27 20:02:43 +0530

    feat: created wsgi development server

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+# wsgi.py
+from webfuzz import create_app
+
+# 'app' is the WSGI callable that Gunicorn looks for.
+app = create_app()
+
+
+if __name__ == "__main__":
+    if app.config["ENV"] == "dev":
+        app.run(host="0.0.0.0", port=8000)
