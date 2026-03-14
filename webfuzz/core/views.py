cf090f9d80764423e6f39f909bb11a4721876de0
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-03-14 10:39:13 +0530

    reformat: modified index view to return context

--- webfuzz/core/views.py
+++ webfuzz/core/views.py
@@ -1,13 +1,30 @@
 """Core views."""
 
 import os
-from flask import current_app, render_template, send_file
+import pandas as pd
+from pathlib import Path
+from flask import current_app, request, render_template, send_file
 from flask.views import MethodView
 
 
 class IndexView(MethodView):
     def get(self):
-        return render_template("index.html")
+        csv_file = request.args.get("csv", None)
+        if csv_file:
+            if Path(
+                os.path.join(current_app.config["FFUF_OUTPUT_PATH"], csv_file)
+            ).is_file():
+                df = pd.read_csv(
+                    os.path.join(current_app.config["FFUF_OUTPUT_PATH"], csv_file)
+                )
+                data = df.to_dict(orient="records")
+                return render_template("index.html", context={"data": data})
+            else:
+                return render_template(
+                    "index.html", context={"error": "CSV file not found."}
+                )
+
+        return render_template("index.html", context={})
 
 
 def serve_favicon_handler():
